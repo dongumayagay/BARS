2b687ea5b75e9125bed5ec6db876b86df7e0ffb7
Chrlyns <cherilynsradam@gmail.com>
2022-11-22 00:53:42 +0800

    added admin header

--- src/routes/(admin side)/+layout.svelte
+++ src/routes/(admin side)/+layout.svelte
@@ -1,8 +1,26 @@
 <script>
-	import Header from '$lib/components/Header.svelte'
 
-	const title = "Welcome Admin!"
+	import Header from "$lib/components/Header.svelte";
+
+
+	const title = "Welcome Admin"
+	const links = [
+		{
+			path: '/admin',
+			name: 'Dashboard'
+		},
+		{
+			path: '/admin/document-appointment-request',
+			name: 'Requests'
+		},
+		{
+			path: '/admin/make-annoucement',
+			name: 'Announcement'
+		}
+	] 
+
 </script>
+<Header {title} {links}/>
 
-<Header {title} />
+
 <slot />
