08d8d3a327ae67a3d48e725942754621c1431c95
KaiKai <jecalaycay17@gmail.com>
2022-11-21 20:18:35 +0800

    added bg color, full screen

--- src/routes/(client side)/+layout.svelte
+++ src/routes/(client side)/+layout.svelte
@@ -1,9 +1,9 @@
-<div class="navbar bg-base-100 text-neutral-content">
-	<a href="/" class="btn btn-ghost normal-case text-xl">Bars</a>
-</div>
-<div class="tabs tabs-boxed gap-8">
-	<a href="/" class="tab tab-lg tab-lifted tab-active">Tab 1</a>
-	<a href="/" class="tab tab-lg tab-lifted ">Tab 2</a>
-	<a href="/" class="tab tab-lg tab-lifted">Tab 3</a>
-</div>
+<script>
+
+	import Header from "$lib/components/Header.svelte";
+
+</script>
+<Header></Header>
+
+
 <slot />
