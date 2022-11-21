34afef23fbb14d3942f82294b54c866775b2f4a6
Don Gumayagay <dngumayagay@gmail.com>
2022-11-21 17:51:41 +0800

    init

---
+++ src/routes/(admin side)/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Navbar from './Navbar.svelte';
+	import Sidebar from './Sidebar.svelte';
+</script>
+
+<div class="drawer drawer-mobile">
+	<input id="admin-sidebar" type="checkbox" class="drawer-toggle" />
+	<div class="drawer-content flex flex-col">
+		<!-- Page content here -->
+		<Navbar />
+		<slot />
+	</div>
+	<Sidebar />
+</div>
---
+++ src/routes/(admin side)/Sidebar.svelte
@@ -0,0 +1,9 @@
+<div class="drawer-side ">
+	<label for="admin-sidebar" class="drawer-overlay" />
+	<ul class="menu p-4 w-80 bg-base-300 text-base-content ">
+		<!-- Sidebar content here -->
+		<li><a href="/admin">Dashboard</a></li>
+		<li><a href="/admin/">Document Requests</a></li>
+		<li class="mt-auto"><a href="/">Home</a></li>
+	</ul>
+</div>
