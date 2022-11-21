996daa462ac34569329e9c65b7a8135098a293e8
KaiKai <jecalaycay17@gmail.com>
2022-11-21 21:14:51 +0800

    reuse header component in admin page

--- src/routes/(admin side)/+layout.svelte
+++ src/routes/(admin side)/+layout.svelte
@@ -1,14 +1,8 @@
 <script>
-	import Navbar from './Navbar.svelte';
-	import Sidebar from './Sidebar.svelte';
+	import Header from '$lib/components/Header.svelte'
+
+	const title = "Welcome Admin!"
 </script>
 
-<div class="drawer drawer-mobile">
-	<input id="admin-sidebar" type="checkbox" class="drawer-toggle" />
-	<div class="drawer-content flex flex-col">
-		<!-- Page content here -->
-		<Navbar />
-		<slot />
-	</div>
-	<Sidebar />
-</div>
+<Header {title} />
+<slot />
