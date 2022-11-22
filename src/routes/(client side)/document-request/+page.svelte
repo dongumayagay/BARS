831fd12129f43a10a45253171f1c571731d4b03c
KaiKai <jecalaycay17@gmail.com>
2022-11-22 13:51:49 +0800

    added basic info form component

---
+++ src/routes/(client side)/document-request/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import InfoForm from "$lib/components/InfoForm.svelte";
+
+
+</script>
+
+
+<section class="py-6 px-4 lg:px-0">
+    <form class="form-control bg-neutral p-4 grid grid-cols-1 lg:grid-cols-2 rounded-lg">
+        <InfoForm class=""/>
+    </form>
+</section>
