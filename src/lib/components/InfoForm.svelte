a922dfac820910895050dde663b92856ecc55085
KaiKai <jecalaycay17@gmail.com>
2022-11-24 00:52:37 +0800

    added required attributes. removed comments

--- src/lib/components/InfoForm.svelte
+++ src/lib/components/InfoForm.svelte
@@ -1,4 +1,8 @@
 <script>
+	import { validate_component } from "svelte/internal";
+
+  // export let lastNameValid ;
+  
 
 </script>
 
@@ -11,77 +15,64 @@
         <div class="flex flex-col flex-1">
             <label for="lastName" class="label">
                 <span class="label-text">Last Name</span>
-                <!-- <span class="label-text-alt">Alt label</span> -->
               </label>
-            <!-- <label for="lastName" class="label">Last name</label> -->
-            <input type="text" id="lastName" placeholder="Type here" class="input input-bordered input-md input-secondary w-full max-w-xs bg-white focus:border-primary focus:outline-offset-[3px]" />
+            <input required title="Please your Last Name" type="text" 
+              id="lastName" name="lastName" placeholder="Type here" 
+              class="input input-bordered input-md invalid:input-error input-secondary w-full max-w-xs bg-white focus:border-primary focus:outline-offset-[3px]"
+              />
         </div>
         <div class="flex flex-col flex-1">
             <label for="firstName" class="label">
                 <span class="label-text">First Name</span>
-                <!-- <span class="label-text-alt">Alt label</span> -->
               </label>
-            <!-- <label for="lastName" class="label">Last name</label> -->
-            <input type="text" id="firstName" placeholder="Type here" class="input input-bordered input-md input-secondary w-full max-w-xs bg-white focus:border-primary focus:outline-offset-[3px]" />
+            <input required type="text" id="firstName" name="firstName" placeholder="Type here" class="input input-bordered input-md input-secondary w-full max-w-xs bg-white focus:border-primary focus:outline-offset-[3px]" />
         </div>
         <div class="flex flex-col flex-1">
             <label for="middleName" class="label">
                 <span class="label-text">Middle Name</span>
-                <!-- <span class="label-text-alt">Alt label</span> -->
               </label>
-            <!-- <label for="lastName" class="label">Last name</label> -->
-            <input type="text" id="lastName" placeholder="Type here" class="input input-bordered input-md input-secondary w-full max-w-xs bg-white focus:border-primary focus:outline-offset-[3px]" />
+            <input required type="text" id="lastName" name="middleName" placeholder="Type here" class="input input-bordered input-md input-secondary w-full max-w-xs bg-white focus:border-primary focus:outline-offset-[3px]" />
         </div>
     </div>
     <div class="flex flex-col flex-1">
         <label for="address" class="label">
             <span class="label-text">Complete Address</span>
-            <!-- <span class="label-text-alt">Alt label</span> -->
           </label>
-        <!-- <label for="lastName" class="label">Last name</label> -->
-        <input type="text" id="address" placeholder="Type here" class="input input-bordered input-md input-secondary w-full bg-white focus:border-primary focus:outline-offset-[3px]" />
+        <input required type="text" id="address" name="address" placeholder="Type here" class="input input-bordered input-md input-secondary w-full bg-white focus:border-primary focus:outline-offset-[3px]" />
     </div>
-    <div class="flex flex-col flex-1 ">
+    <div class="group flex flex-col flex-1 ">
         <label for="email" class="label">
             <span class="label-text">Email Address</span>
-            <!-- <span class="label-text-alt">Alt label</span> -->
           </label>
-        <!-- <label for="lastName" class="label">Last name</label> -->
-        <input type="email" id="email" placeholder="example@email.com" class="input input-bordered input-md input-secondary w-full bg-white focus:border-primary focus:outline-offset-[3px]" />
+        <input required type="email" id="email" name="email" placeholder="example@email.com" class="input input-bordered input-md invalid:input-error input-secondary w-full bg-white focus:border-primary focus:outline-offset-[3px]"/>
     </div>
     <section class="flex flex-row gap-2">
         <div class="flex flex-col flex-1 ">
             <label for="birthDate" class="label">
                 <span class="label-text">Birthdate</span>
-                <!-- <span class="label-text-alt">Alt label</span> -->
               </label>
-            <!-- <label for="lastName" class="label">Last name</label> -->
-            <input type="date" id="birthdate" placeholder="Type here" class="input input-bordered input-md input-secondary w-full bg-white focus:border-primary focus:outline-offset-[3px]" />
+            <input required type="date" id="birthdate" name="birthdate" placeholder="Type here" class="input input-bordered input-md input-secondary w-full bg-white focus:border-primary focus:outline-offset-[3px]" />
         </div>
         <div class="flex flex-col flex-1 ">
             <label for="age" class="label">
                 <span class="label-text">Age</span>
                 <span class="label-text-alt">years old</span>
               </label>
-            <!-- <label for="lastName" class="label">Last name</label> -->
-            <input type="text" id="birthdate" placeholder="Type here" class="input input-bordered input-md input-secondary w-full bg-white focus:border-primary focus:outline-offset-[3px]" />
+            <input required type="number" id="age" name="age" placeholder="Type here" class="input input-bordered input-md input-secondary w-full bg-white focus:border-primary focus:outline-offset-[3px]" />
         </div>
     </section>
     <div class="flex flex-col flex-1 ">
         <label for="contact" class="label">
             <span class="label-text">Phone Number</span>
-            <!-- <span class="label-text-alt">Alt label</span> -->
           </label>
-        <!-- <label for="lastName" class="label">Last name</label> -->
-        <input type="text" id="contact" placeholder="Type here" class="input input-bordered input-md input-secondary w-full bg-white focus:border-primary focus:outline-offset-[3px]"/>
+        <input required type="tel" id="contact" name="contact" placeholder="09** *** *** / 639** *** ****" class="input input-bordered input-md input-secondary w-full bg-white focus:border-primary focus:outline-offset-[3px]" />
     </div>
     <div class="flex flex-col flex-1 ">
-        <label for="contact" class="label">
+        <label for="purpose" class="label">
             <span class="label-text">Purpose of the Request</span>
-            <!-- <span class="label-text-alt">Alt label</span> -->
           </label>
-        <!-- <label for="lastName" class="label">Last name</label> -->
-        <input type="text" id="contact" placeholder="Type here" class="input input-bordered input-md input-secondary w-full bg-white focus:border-primary focus:outline-offset-[3px]"/>
+        <input required type="text" id="purpose" name="purpose" placeholder="Type here" class="input input-bordered input-md input-secondary w-full bg-white focus:border-primary focus:outline-offset-[3px]"/>
     </div>
     
 </section>
+
