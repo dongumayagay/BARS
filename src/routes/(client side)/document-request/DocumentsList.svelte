6e2cbb15bd2d6dba20b34d09ca219971bb82f2a2
KaiKai <jecalaycay17@gmail.com>
2022-11-26 15:20:10 +0800

    changed directory to document request

---
+++ src/routes/(client side)/document-request/DocumentsList.svelte
@@ -0,0 +1,92 @@
+<script>
+    import {createEventDispatcher} from "svelte";
+    const documentsList = [
+            {
+                id: "1",
+                name: "Document 1",
+                requirements: [
+                    {requirement: "requirement1"},
+                    {requirement: "requirement2"},
+                ]
+            },
+            {
+                id: "2",
+                name: "Document 2",
+                requirements: [
+                    {requirement: "requirement1"},
+                    {requirement: "requirement2"},
+                    {requirement: "requirement3"}
+                ]
+            },
+            {
+                id: "3",
+                name: "Document 3",
+                requirements: [
+                    {requirement: "requirement1"}
+                ]
+            },
+            {
+                id: "4",
+                name: "Document 4",
+                requirements: [
+                    {requirement: "requirement1"},
+                    {requirement: "requirement2"},
+                    {requirement: "requirement3"}
+                ]
+            },
+        ]
+    let documentsRequestList = []
+    const dispatch = createEventDispatcher()
+    function submitHandler() {
+        dispatch("next", {
+            documentsRequestList
+        })
+    }
+</script>
+
+<svelte:head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" 
+			integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" 
+			crossorigin="anonymous" referrerpolicy="no-referrer" />
+</svelte:head>
+
+
+<form class="flex flex-col items-center justify-start gap-3" on:submit|preventDefault={submitHandler} on:reset={(event)=>event.target.reset()}>
+    <div class="w-full flex justify-start">
+        <button type="button" class="lg:fixed lg:left-[15%] lg:bottom-[55%] btn btn-glass flex gap-2" on:click={()=>dispatch("back")}>
+            <i class="fa-solid fa-arrow-left"></i>
+            <p>Go Back</p>
+        </button>
+    </div>
+    <div>
+        <p class="font-bold">Documents List</p>
+    </div>
+    <p>Kindly check the box of the documents you need to request</p>
+    <div class="flex flex-col items-start w-[95%] gap-3">
+        {#each documentsList as document}
+            <div class="w-full flex flex-col gap-2">
+                <div class="flex items-center justify-center gap-3 ">
+                    <input type="checkbox" 
+                        value={document}
+                        name="documents"
+                        class="checkbox checkbox-primary"
+                        bind:group={documentsRequestList} 
+                    />
+                    <p class="w-full bg-primary p-2 rounded-xl">{document.name}</p>
+                </div>
+                <div class="flex flex-col items-start pl-14 gap-2">
+                    {#each document.requirements as requirement}
+                        <p>{requirement.requirement}</p>
+                    {/each}
+                </div>
+            </div>
+        {/each}
+    </div>
+    <section class="w-full flex justify-start gap-4">
+        <button type="submit" class="btn btn-primary flex gap-2" disabled={documentsRequestList.length === 0}>
+            <p>Next</p> 
+            <i class="fa-solid fa-arrow-right"></i>
+        </button>
+        <button type="reset">Clear Form</button>
+    </section>
+</form>
