eecf73e55c4967062dda3ef1297bc0695d800329
KaiKai <jecalaycay17@gmail.com>
2022-11-27 22:06:36 +0800

    created and imported AfterSubmit

--- src/routes/(client side)/document-request/+page.svelte
+++ src/routes/(client side)/document-request/+page.svelte
@@ -1,8 +1,9 @@
 <script>
-    import InfoForm from "$lib/components/InfoForm.svelte"
-    import DocumentsList from "./DocumentsList.svelte"
-    import FileUpload from "./FileUpload.svelte"
-    import RequestInfo from "./RequestInfo.svelte"
+    import InfoForm from "$lib/components/InfoForm.svelte";
+    import DocumentsList from "./DocumentsList.svelte";
+    import FileUpload from "./FileUpload.svelte";
+    import RequestInfo from "./RequestInfo.svelte";
+    import AfterSubmit from "./AfterSubmit.svelte";
     import { firestore,  storage } from "$lib/firebase/client.js";
     import { doc, setDoc, Timestamp, collection } from "firebase/firestore";
     import { ref, uploadBytes } from "firebase/storage";
@@ -16,17 +17,11 @@
     let requirementsFiles = []
 
     function nextHandler(event) {
-        documentRequest = Object.assign(documentRequest, event.detail)
         if(!!event.detail.filesToUpload){
             requirementsFiles = {...requirementsFiles, ...event.detail.filesToUpload}
             console.log(requirementsFiles)
         }
-        console.log(documentRequest)
         page += 1;
-
-        // if(!!documentRequest.contactInfo && !!documentRequest.documentsRequestList && !!requirementsFiles.length){
-        //     formCompleted = true;
-        // }
     }
 
     async function submitToDatabase(){
@@ -68,10 +63,8 @@
                     console.log('request successfully added to the database' + requestId)
                 )
                 requestCompleted = true;
-                console.log(requestCompleted)
             } catch (error) {
                 const errorMessage = error.errorMessage;
-                console.log(errorMessage);
             }
     }
 
@@ -85,40 +78,48 @@
 </svelte:head>
 
 <div class="p-3 flex flex-col items-center gap-4">
-    <ul class="steps lg:w-[75%]">
-        <li class="step font-semibold" class:step-success={page >= 0}>Contact Info</li>
-        <li class="step {page >= 1 ? "step-success text-accent font-semibold": "text-black/50"}">Documents to Request</li>
-        <li class="step {page >= 2 ? "step-success text-accent font-semibold": "text-black/50"}">Upload Requirements</li>
-        <li class="step {page === 3 ? "step-success text-accent font-semibold": "text-black/50"}">Confirm</li>
-    </ul>
-    
-    <div  class="w-[90%] lg:w-[45%] p-4 lg:px-10 bg-neutral rounded-xl flex flex-col justify-center shadow-xl gap-3" class:hidden={page !== 0}>
-        <InfoForm on:next={nextHandler}/>
-    </div>
-    
-    <div class="w-[95%] lg:w-[45%] p-4 bg-neutral rounded-xl flex justify-start shadow-xl" class:hidden={page !== 1}>
-        <DocumentsList on:next={nextHandler} on:back={()=>page -= 1 }/>
-    </div>
-
-    <div class="w-[95%] lg:w-[45%] p-4 lg:px-6 bg-neutral rounded-xl flex justify-center shadow-xl" class:hidden={page !== 2}>
-        <FileUpload doclistReq={documentRequest?.documentsRequestList??[]} on:next={nextHandler} on:back={()=>page -= 1 }/>
+    {#if !requestCompleted}
+        <ul class="steps lg:w-[75%]">
+            <li class="step font-semibold" class:step-success={page >= 0}>Contact Info</li>
+            <li class="step {page >= 1 ? "step-success text-accent font-semibold": "text-black/50"}">Documents to Request</li>
+            <li class="step {page >= 2 ? "step-success text-accent font-semibold": "text-black/50"}">Upload Requirements</li>
+            <li class="step {page === 3 ? "step-success text-accent font-semibold": "text-black/50"}">Confirm</li>
+        </ul>
         
-    </div>
+        <div  class="w-[90%] lg:w-[45%] p-4 lg:px-10 bg-neutral rounded-xl flex flex-col justify-center shadow-xl gap-3" class:hidden={page !== 0}>
+            <InfoForm on:next={nextHandler}/>
+        </div>
+        
+        <div class="w-[95%] lg:w-[45%] p-4 bg-neutral rounded-xl flex justify-start shadow-xl" class:hidden={page !== 1}>
+            <DocumentsList on:next={nextHandler} on:back={()=>page -= 1 }/>
+        </div>
 
-    <div class="flex flex-col gap-4 justify-center" class:hidden={page !== 3}>
-        <div class="w-[95%] lg:w-max p-4 lg:px-6 bg-neutral rounded-xl flex justify-center shadow-xl" >
-            <RequestInfo 
-                contactInfo={documentRequest?.contactInfo??[]} 
-                documentsRequestList={documentRequest?.documentsRequestList??[]}
-                on:backToPageZero = {() => page = 0}
-                on:backToPageOne = {() => {
-                    page = 1; 
-                    requirementsFiles = [];
-                }}
-            />
+        <div class="w-[95%] lg:w-[45%] p-4 lg:px-6 bg-neutral rounded-xl flex justify-center shadow-xl" class:hidden={page !== 2}>
+            <FileUpload doclistReq={documentRequest?.documentsRequestList??[]} on:next={nextHandler} on:back={()=>page -= 1 }/>
+            
         </div>
-        <div class="w-[95%] lg:w-[65%] flex justify-center">
-            <button type="button" class="btn btn-success" on:click={submitToDatabase}>Submit Request</button>
+
+        <div class="flex flex-col gap-4 justify-center" class:hidden={page !== 3}>
+            <div class="w-[95%] lg:w-max p-4 lg:px-6 bg-neutral rounded-xl flex justify-center shadow-xl" >
+                <RequestInfo 
+                    contactInfo={documentRequest?.contactInfo??[]} 
+                    documentsRequestList={documentRequest?.documentsRequestList??[]}
+                    on:backToPageZero = {() => page = 0}
+                    on:backToPageOne = {() => {
+                        page = 1; 
+                        requirementsFiles = [];
+                    }}
+                />
+            </div>
+            <div class="w-[95%] lg:w-[65%] flex justify-center">
+                <button type="button" class="btn btn-success" on:click={submitToDatabase}>Submit Request</button>
+            </div>
         </div>
-    </div>
+    {:else}
+        <section>
+            <AfterSubmit {requestId} />
+    
+        </section>
+    {/if}
 </div>
+
---
+++ src/routes/(client side)/document-request/AfterSubmit.svelte
@@ -0,0 +1,26 @@
+<script>
+
+    export let requestId;
+    
+    let textCopied = false;
+</script>
+
+<div id="afterSubmit" class="lg:h-[300px] bg-neutral p-3 border-2 border-white flex flex-col items-center justify-center gap-3 shadow-lg rounded-md">
+    <p class="w-[75%] lg:text-md text-center">Thank you for submitting your request, you will be notified through email if your document is ready.</p>
+    <small class="text-center">Below is your request id. Please write it down or press the button to copy it to your device's clipboard.</small>
+    <div class="flex items-center w-full justify-center">
+        <p id="requestID" class="w-max font-bold p-3 mr-2 text-accent rounded-xl hover:underline">{requestId}</p>
+        <button 
+            title="Press to copy your request ID" 
+            class="p-2 gap-1 flex items-center btn btn-primary btn-sm" 
+            on:click={() => {
+                navigator.clipboard.writeText(requestId);
+                textCopied = true;
+                setTimeout(() => {textCopied = false}, 2000)
+                }}>
+            <p class="text-[10px]">{textCopied ? "Copied": "Copy"}</p>
+            <i class="fa-regular fa-copy"></i>
+        </button>
+    </div>
+        <button type="button" class="btn btn-ghost btn-sm hover:bg-neutral hover:underline" on:click={() => window.location.reload()}>Finish</button>
+</div>
