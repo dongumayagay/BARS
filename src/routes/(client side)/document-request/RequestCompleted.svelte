2ad795313a54cb84378153f9df77561e53d6f86c
KaiKai <jecalaycay17@gmail.com>
2022-11-30 12:48:35 +0800

    rename, added a redirect button to request tracker

--- src/routes/(client side)/document-request/RequestCompleted.svelte
+++ src/routes/(client side)/document-request/RequestCompleted.svelte
@@ -1,4 +1,5 @@
-<!-- <script>
+<script>
+    import {goto} from "$app/navigation"
 
     export let requestId;
     
@@ -7,11 +8,11 @@
 
 <div id="afterSubmit" class="lg:h-[300px] bg-neutral p-3 border-2 border-white flex flex-col items-center justify-center gap-3 shadow-lg rounded-md">
     <p class="w-[75%] lg:text-md text-center">Thank you for submitting your request, you will be notified through email if your document is ready.</p>
-    <small class="text-center">Below is your request id. Please write it down or press the button to copy it to your device's clipboard.</small>
+    <small class="text-center">Below is your request id. An email is also sent to you containing the link to your request's tracker.</small>
     <div class="flex items-center w-full justify-center">
         <p id="requestID" class="w-max font-bold p-3 mr-2 text-accent rounded-xl hover:underline">{requestId}</p>
         <button 
-            title="Press to copy your request ID" 
+            title="Copy your request ID" 
             class="p-2 gap-1 flex items-center btn btn-primary btn-sm" 
             on:click={() => {
                 navigator.clipboard.writeText(requestId);
@@ -22,5 +23,5 @@
             <i class="fa-regular fa-copy"></i>
         </button>
     </div>
-        <button type="button" class="btn btn-ghost btn-sm hover:bg-neutral hover:underline" on:click={() => window.location.reload()}>Finish</button>
-</div> -->
+        <button type="button" class="btn btn-ghost btn-sm hover:bg-neutral hover:underline" on:click={() => goto('./document-request/' + requestId)}>Go to tracker</button>
+</div>
