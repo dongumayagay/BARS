a76f1182ed8cc9a5404a87914198330dbd978fad
KaiKai <jecalaycay17@gmail.com>
2022-12-03 01:02:58 +0800

    added confirm and request complete component

--- src/routes/(client side)/appointment-request/+page.svelte
+++ src/routes/(client side)/appointment-request/+page.svelte
@@ -1,12 +1,14 @@
 <script>
     import DatePicker from "./DatePicker.svelte";
-    import InfoForm from "../../../lib/components/InfoForm.svelte";
+    import InfoForm from "$lib/components/InfoForm.svelte";
+    import RequestCompleted from "$lib/components/RequestCompleted.svelte"; 
 	import OfficialsList from "./OfficialsList.svelte";
 	import ConfirmAppointment from "./ConfirmAppointment.svelte";
     import {db} from '$lib/firebase/client.js'
     import {addDoc, collection, Timestamp} from 'firebase/firestore';
 
-    let page = 1;
+    let page = 0;
+    let requestSubmitted = false;
 
     let appointmentRequest = [];
     let requestId;
@@ -32,11 +34,13 @@
                 appointmentDate: appointmentRequest.selectedDateAndTime.date,
                 appointmentTime: appointmentRequest.selectedDateAndTime.time,
                 appointmentPurpose: appointmentRequest.contactInfo.purpose,
+                selectedOfficial: appointmentRequest.selectedOfficial,
                 status: "pending"
             })
 
             requestId = appointmentRequestRef.id;
             alert('Your request for an appointment has been submitted\n\nRequest ID: ' + appointmentRequestRef.id);
+            requestSubmitted = true; 
         } catch (error) {
             console.log(error)
         }
@@ -44,33 +48,43 @@
 
 </script>
 
-<div class="p-3 flex flex-col items-center gap-4">
-    <ul class="steps w-full lg:w-[75%]">
-        <li class="step font-semibold {page != 0 ? 'text-black/50' : ''}" class:step-success={page >= 0} class:text-sm={page!==0}>Contact Info</li>
-        <li class="step {page >= 1 ? "step-success font-semibold": ""} {page != 1 ? 'text-black/50' : ''}" class:text-sm={page!==1}>Pick date & time</li>
-        <li class="step {page >= 2 ? "step-success font-semibold": ""} {page != 2 ? 'text-black/50' : ''}" class:text-sm={page!==2}>Select an official</li>
-        <li class="step {page === 3 ? "step-success font-semibold": ""} {page != 3 ? 'text-black/50' : ''}" class:text-sm={page!==3}>Confirm</li>
-    </ul>
+{#if !requestSubmitted}
+    <section class="p-3 flex flex-col items-center gap-4">
+        <ul class="steps w-full lg:w-[75%]">
+            <li class="step font-semibold {page != 0 ? 'text-black/50' : ''}" class:step-success={page >= 0} class:text-sm={page!==0}>Contact Info</li>
+            <li class="step {page >= 1 ? "step-success font-semibold": ""} {page != 1 ? 'text-black/50' : ''}" class:text-sm={page!==1}>Pick date & time</li>
+            <li class="step {page >= 2 ? "step-success font-semibold": ""} {page != 2 ? 'text-black/50' : ''}" class:text-sm={page!==2}>Select an official</li>
+            <li class="step {page === 3 ? "step-success font-semibold": ""} {page != 3 ? 'text-black/50' : ''}" class:text-sm={page!==3}>Confirm</li>
+        </ul>
 
-    <div  class="w-[90%] lg:w-[45%] p-4 lg:px-10 bg-neutral rounded-xl flex flex-col justify-center shadow-xl gap-3" class:hidden={page !== 0}>
-        <InfoForm on:next={nextHandler}/>
-    </div>
-
-    <div class="w-[95%] lg:w-max p-4 bg-neutral rounded-xl shadow-xl" class:hidden={page !== 1}>
-        <DatePicker on:next={nextHandler} on:back={()=>{page -= 1}}/>
-    </div>
-
-    <div class="w-[95%] lg:w-max p-4 bg-neutral rounded-xl shadow-xl" class:hidden={page !== 2}>
-        <OfficialsList on:next={nextHandler} on:back={()=>{page -= 1}}/>
-    </div>
-
-    <div class="w-full flex justify-center" class:hidden={page !== 3}>
-        <div class="w-[95%] lg:w-max p-4 lg:px-6 bg-neutral rounded-xl flex justify-center shadow-xl" >
-            <ConfirmAppointment 
-                {appointmentRequest}
-                on:submit={submitToDatabase}
-            />
+        <div  class="w-[90%] lg:w-[45%] p-4 lg:px-10 bg-neutral rounded-xl flex flex-col justify-center shadow-xl gap-3" class:hidden={page !== 0}>
+            <InfoForm on:next={nextHandler}/>
         </div>
-    </div>
 
-</div>
+        <div class="w-[95%] lg:w-max p-4 bg-neutral rounded-xl shadow-xl" class:hidden={page !== 1}>
+            <DatePicker on:next={nextHandler} on:back={()=>{page -= 1}}/>
+        </div>
+
+        <div class="w-[95%] lg:w-max p-4 bg-neutral rounded-xl shadow-xl" class:hidden={page !== 2}>
+            <OfficialsList on:next={nextHandler} on:back={()=>{page -= 1}}/>
+        </div>
+
+        <div class="w-full flex justify-center" class:hidden={page !== 3}>
+            <div class="w-[95%] lg:w-max p-4 lg:px-6 bg-neutral rounded-xl flex justify-center shadow-xl" >
+                <ConfirmAppointment 
+                    {appointmentRequest}
+                    on:back={()=>{page -= 1}}
+                    on:submit={submitToDatabase}
+                />
+            </div>
+        </div>
+
+    </section>
+
+{:else}
+
+    <section class="pt-4">
+        <RequestCompleted {requestId} trackerPath='./appointment-request/'/>
+    </section>
+
+{/if}
