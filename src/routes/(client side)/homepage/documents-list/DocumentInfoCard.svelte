f0b00b5ad88e878c16a098393074f20b649739ad
KaiKai <jecalaycay17@gmail.com>
2022-12-20 13:08:12 +0800

    added shadow on cards and dropdown components

--- src/routes/(client side)/homepage/documents-list/DocumentInfoCard.svelte
+++ src/routes/(client side)/homepage/documents-list/DocumentInfoCard.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <section class="w-[80%] flex flex-col gap-2">
-    <section class="w-full bg-neutral flex flex-col rounded-xl z-10">
+    <section class="w-full bg-neutral flex flex-col rounded-xl shadow-lg z-10">
         <div class="w-full h-[75px] border-b-[1px] border-primary p-4">
             <p class="font-semibold">{document.name}</p>
         </div>
@@ -29,7 +29,7 @@
     
     <section class="w-full flex flex-col gap-2 rounded-xl z-1 mb-10 {showList ? " " : "opacity-0 h-0  translate-y-[-6rem]"} transition-all ease-in-out duration-300 z-1">
         {#each document.requirements as requirement}
-            <div class="bg-neutral flex items-center gap-4 rounded-xl p-4">
+            <div class="bg-neutral flex items-center gap-4 rounded-xl shadow-lg p-4">
                 <i class="fa-solid fa-id-card"></i>
                 <div class="flex flex-col">
                     <p>{requirement.name}</p>
--- src/routes/(client side)/homepage/officials/Official.svelte
+++ src/routes/(client side)/homepage/officials/Official.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <section class="w-[80%] flex flex-col gap-2">
-    <section class="w-full bg-neutral flex flex-col rounded-xl z-10">
+    <section class="w-full bg-neutral flex flex-col rounded-xl shadow-lg z-10">
         <div class="w-full h-[100px] border-b-[1px] border-primary p-4">
             <p class="font-semibold">{officialProfile.name}</p>
             <p class="text-sm ">{officialProfile.position}</p>
@@ -31,7 +31,7 @@
     
     <section class="w-full flex flex-col gap-2 rounded-xl z-1 mb-10 {showContacts ? " " : "opacity-0 h-0  translate-y-[-6rem]"} transition-all ease-in-out duration-300 z-1">
         {#if officialProfile.email != ""}
-            <a href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcRzDsPgrvpFkBRKMWflTMCNScvkfdBcVRGnFhFhcLLXjFcCZBLRrFGWTRknCzDCXwdQPWwxH/${officialProfile.email}" class="bg-neutral flex items-center gap-4 rounded-xl p-4">
+            <a href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcRzDsPgrvpFkBRKMWflTMCNScvkfdBcVRGnFhFhcLLXjFcCZBLRrFGWTRknCzDCXwdQPWwxH" class="bg-neutral flex items-center gap-4 rounded-xl p-4">
                 <!-- <i class="fa-solid fa-envelope text-red-600 text-[45px]"></i> -->
                 <i class="fa-regular fa-envelope text-red-600 text-[45px]"></i>
                 <div class="flex flex-col">
@@ -41,7 +41,7 @@
             </a>
         {/if}
         {#if officialProfile.facebook != ""}
-            <a href={officialProfile.facebook.link} class="bg-neutral flex items-center gap-4 rounded-xl p-4">
+            <a href={officialProfile.facebook.link} class="bg-neutral flex items-center gap-4 rounded-lg shadow-lg p-4">
                 <i class="fa-brands fa-facebook text-[45px] text-blue-900"></i>
                 <div class="flex flex-col">
                     <p class="font-semibold text-blue-900 text-lg">Facebook</p>
