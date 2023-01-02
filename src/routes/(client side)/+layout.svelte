faf754bc0a5270fee54566551112db3fe8fa779e
KaiKai <jecalaycay17@gmail.com>
2023-01-02 12:06:33 +0800

    revert changes

--- src/routes/(client side)/+layout.svelte
+++ src/routes/(client side)/+layout.svelte
@@ -2,8 +2,28 @@
 
 	import Header from "$lib/components/Header.svelte";
 
+	const title = 'Welcome to Barangay Appointment and Request System';
+	const links = [
+		{
+			path: './',
+			name: 'Homepage'
+		},
+		{
+			path: '/document-request',
+			name: 'Request Documents'
+		},
+		{
+			path: '/appointment-request',
+			name: 'Make an Appointment'
+		},
+		{
+			path: '/ticket-tracker',
+			name: 'Ticket Tracker'
+		}
+	]
+
 </script>
-<Header/>
+<Header {title} {links}/>
 
 
 <slot />
