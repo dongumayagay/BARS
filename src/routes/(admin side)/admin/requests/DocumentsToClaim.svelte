22c3305bed80568a1529f6dd74887640c2ed3ae1
KaiKai <jecalaycay17@gmail.com>
2023-01-03 12:35:38 +0800

    created claimableDocs page, code from pending list

--- src/routes/(admin side)/admin/requests/DocumentsToClaim.svelte
+++ src/routes/(admin side)/admin/requests/DocumentsToClaim.svelte
@@ -1,8 +1,53 @@
 <script>
+    import RequestPreview from "../../../../lib/components/RequestPreview.svelte";
+    import RequestViewer from "./RequestViewer.svelte";
+	import { onSnapshot, query, collection, where, doc } from "firebase/firestore";
+    import { db } from "$lib/firebase/client.js"
+
 
     export let page;
+
+    let dataToView = {};
+    let viewing  = false;
+
+    let readyToClaimDocuments = [];
+
+    const unsubscribe = onSnapshot(query(collection(db, "documentRequests"), where("status", "==", "Ready to claim")), (querySnapshot) => {
+        readyToClaimDocuments = [];
+        querySnapshot.forEach((doc) => {
+            readyToClaimDocuments = [ ...readyToClaimDocuments, {
+                ...doc.data(),
+                requestId: doc.id,
+                typeOfRequest: "Document Request",
+                collectionReference: "documentRequests",
+                nextStatus: "Request Completed",
+                nextStatusEmailContent: "Good Day! Your requested document/s are now claimed. We will mark this request as completed. Thank you for using our system!!"
+            }]
+        })
+    })
+
+    function viewHandler(event) {
+        dataToView = event.detail.requestData;
+        viewing = true;
+    }
+
+    function closeHandler() {
+        dataToView = {};
+        viewing = false;
+    }
+
 </script>
 
-<div class="w-full flex justify-center" class:hidden={page !== 1}>
-    <p class="w-max h-max text-center mb-4 p-2 bg-primary rounded-lg">TO CLAIM DOCUMENTS</p>
+<div class="w-full flex flex-col items-center" class:hidden={page !== 1}>
+    <div class="w-full flex flex-col items-center py-6" class:hidden={viewing}>
+        <p class="w-max h-max text-center mb-4 p-2 rounded-lg font-bold">Ready to claim documents</p>
+        <div class=" overflow-y-auto max-h-[400px] flex flex-col gap-4">
+            {#each readyToClaimDocuments as readyToClaimDocument }
+                <RequestPreview requestData={readyToClaimDocument} on:view={viewHandler}/>
+            {/each}
+        </div>
+    </div>
+    <div class="w-full" class:hidden={!viewing}>
+        <RequestViewer {dataToView} nextStatus={dataToView.nextStatus} on:close={closeHandler}/>
+    </div>
 </div>
