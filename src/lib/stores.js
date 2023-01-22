2b28e33328fd1f37ebbeaad478447cbcc898f53f
KaiKai-dev <jecalaycay17@gmail.com>
2023-01-22 11:57:11 +0800

    added months list for future date formatting

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -14,3 +14,19 @@ export const userStore = readable(undefined, set => {
     )
     return () => unsubscribe()
 })
+
+
+export const months = [
+    "January", 
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+]
