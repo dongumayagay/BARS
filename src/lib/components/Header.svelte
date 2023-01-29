61ee0bdddfbcb90fae9df62e0aa808340592ec24
KaiKai-dev <jecalaycay17@gmail.com>
2023-01-30 04:03:48 +0800

    Added logout and settings button, signOut()

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,6 +1,9 @@
 <script>
-	import {currentPage} from "$lib/stores.js";
+	import {currentPage,userStore} from "$lib/stores.js";
+	import {auth} from "$lib/firebase/client.js"
 	import { goto } from '$app/navigation'
+	import { createEventDispatcher } from "svelte";
+	import { signOut } from "firebase/auth";
 
 	export let title = "Welcome to Barangay Appointment and Request System"
 	export let links = [
@@ -11,7 +14,19 @@
 		
 	] 
 
+	const dispatch = createEventDispatcher();
+
 	let showMenu = false;
+
+	function logOutHandler(){
+		signOut(auth)
+		.then(()=>{
+			alert("Logged out successfully")
+			dispatch("logout")
+		}).catch((error) => {
+			alert(error.message)
+		});
+	}
 </script>
 
 
@@ -23,7 +38,29 @@
 	</section>
 	<section class="flex flex-col flex-1 ">
 		<!--  -->
-		<h1 class=" text-center flex items-center justify-center flex-1 font-serif text-sm lg:text-xl">{title}</h1>
+		<div class="w-full h-full flex {$userStore === undefined || $userStore === null ? "justify-center" : "justify-end"} p-2">
+			<h1 class="w-full h-full text-center flex items-center justify-center flex-1 font-serif text-sm lg:text-xl">{title}</h1>
+			{#if !!$userStore}
+			<div class="dropdown dropdown-end">
+				<button class="btn btn-sm w-max h-max">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+						<path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+					</svg>
+				</button>
+				<button class="dropdown-content menu shadow-lg bg-neutral rounded-box w-max h-max">
+					<li>
+						<button class="flex justify-center hover:bg-neutral hover:underline" on:click={logOutHandler}>
+							<small>Log Out</small>
+						</button>
+					</li>
+				  <li><button class="flex justify-center hover:bg-neutral hover:underline">
+						<small>Settings</small>
+				  </button></li>
+				  <!-- <li><a>Item 2</a></li> -->
+				</button>
+			  </div>
+			{/if}
+		</div>
 
 		<nav class="tabs items-end flex-1 hidden lg:flex justify-evenly ">
 			{#each links as link, index}
