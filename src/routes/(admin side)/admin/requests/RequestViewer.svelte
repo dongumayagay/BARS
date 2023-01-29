e9c95a0bae1c2648b502677c4df6eb414435ebc4
KaiKai-dev <jecalaycay17@gmail.com>
2023-01-29 22:58:19 +0800

    add data to log, history log when a req is trashed

--- src/routes/(admin side)/admin/requests/RequestViewer.svelte
+++ src/routes/(admin side)/admin/requests/RequestViewer.svelte
@@ -22,6 +22,7 @@
                 await addDoc(collection( db, "history"), {
                     previousStatus: dataToView.status,
                     currentStatus: dataToView.previousStatus,
+                    typeOfRequest: dataToView.typeOfRequest,
                     logDate: Timestamp.now(),
                     requestId: dataToView.requestId,
                 })
@@ -37,6 +38,7 @@
                 await addDoc(collection( db, "history"), {
                     previousStatus: dataToView.status,
                     currentStatus: dataToView.nextStatus,
+                    typeOfRequest: dataToView.typeOfRequest,
                     logDate: Timestamp.now(),
                     requestId: dataToView.requestId,
                 })
@@ -66,7 +68,15 @@
         try {
             const docRef = doc(db, dataToView.collectionReference, dataToView.requestId);
 
-            const docUpdate = await updateDoc(docRef, {
+            await addDoc(collection( db, "history"), {
+                previousStatus: dataToView.status,
+                status: "Trashed",
+                typeOfRequest: dataToView.typeOfRequest,
+                logDate: Timestamp.now(),
+                requestId: dataToView.requestId,
+            })
+
+            await updateDoc(docRef, {
                 previousStatus: dataToView.status,
                 status: "Trashed",
             })
@@ -87,7 +97,7 @@
             <p class="hover:underline">Close</p>
         </button>
         <div class="flex gap-2">
-            {#if dataToView.status !== 'Trashed'}
+            {#if dataToView.status !== 'Trashed' && dataToView.nextStatus !== undefined}
             <button class="btn btn-success btn-sm text-sm hover:underline hover:scale-105 active:scale-100" on:click={updateHandler}>Update Status to: {dataToView.nextStatus}</button>
             <button class="btn btn-error btn-sm flex gap-2 hover:scale-105 active:scale-100" on:click={trashHandler}>
                 <p>Move to Trash</p>
@@ -95,7 +105,8 @@
                     <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                 </svg>
             </button>
-            {:else}
+            {:else if dataToView.status === 'Trashed' }
+            <!-- && dataToView.nextStatus !== undefined -->
             <button class="btn btn-info btn-sm text-sm hover:underline hover:scale-105 active:scale-100" on:click={updateHandler}>Revert Status to: {dataToView?.previousStatus}</button>
             {/if}
         </div>
