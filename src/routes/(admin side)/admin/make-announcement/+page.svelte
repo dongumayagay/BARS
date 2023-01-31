272fe12263d7e4e0f02723aefbbc55fb2848775d
KaiKai-dev <jecalaycay17@gmail.com>
2023-02-01 02:24:35 +0800

    admin side announcement maker setup

--- src/routes/(admin side)/admin/make-announcement/+page.svelte
+++ src/routes/(admin side)/admin/make-announcement/+page.svelte
@@ -5,9 +5,15 @@
 
     onMount(()=>{
         if(!$userStore){
-            $currentPage = 0
+            $currentPage = 2
             goto("../admin")
         }
 
     })
 </script>
+
+<main class="w-full h-screen pt-6">
+    <section class="w-full min-h-[450px] bg-neutral p-6">
+        Post an announcement
+    </section>
+</main>
