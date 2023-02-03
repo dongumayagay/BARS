c16d31119b06e1b69870a4cbed4c5ea7c4560611
KaiKai-dev <jecalaycay17@gmail.com>
2023-02-03 12:43:12 +0800

    announcement maker setup

--- src/routes/(admin side)/admin/make-announcement/+page.svelte
+++ src/routes/(admin side)/admin/make-announcement/+page.svelte
@@ -1,7 +1,10 @@
 <script>
     import { userStore, currentPage } from '$lib/stores.js'
+    import { db } from '$lib/firebase/client.js'
     import {goto} from '$app/navigation';
     import { onMount } from "svelte";
+	import { onSnapshot, query, where, collection } from 'firebase/firestore';
+	import Announcement from './Announcement.svelte';
 
     onMount(()=>{
         if(!$userStore){
@@ -10,10 +13,29 @@
         }
 
     })
+
+    let postedAnnouncements = [];
+
+    const postedAnnouncementsFetcher = onSnapshot(query(collection(db, "announcements"), where("postedBy", "==", $userStore?.email??[])), (querySnapshot)=>{
+        postedAnnouncements = [];
+        querySnapshot.forEach((doc)=>{
+            postedAnnouncements = [...postedAnnouncements, {
+                ...doc.data()
+            }]
+        })
+    })
 </script>
 
 <main class="w-full h-screen pt-6">
-    <section class="w-full min-h-[450px] bg-neutral p-6">
-        Post an announcement
+    <section class="w-full min-h-[450px] bg-neutral flex flex-col p-6 gap-6 rounded-lg shadow-lg">
+        <button class="w-max btn btn-primary flex gap-2">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+            </svg>
+            <p>Post an announcement</p>
+        </button>
+        {#if postedAnnouncements.length !== 0}
+            <Announcement announcements={postedAnnouncements}/> 
+        {/if}
     </section>
 </main>
---
+++ src/routes/(admin side)/admin/make-announcement/Announcement.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { Timestamp } from "firebase/firestore";
+    
+    export let announcements;
+</script>
+
+<div class="w-full flex flex-col gap-2">
+    <p>Your posts:</p>
+    {#each announcements as announcement}
+        <section class="bg-base-100 w-full lg:w-full p-4 flex flex-col rounded-xl shadow-lg">
+            <div class="w-full pb-4 flex flex-col border-b-[2px] border-neutral">
+                <p class="text-xs lg:text-sm">{new Timestamp(announcement.datePosted.seconds, announcement.datePosted.nanoseconds).toDate()}</p>
+                <p class="text-xs lg:text-sm">Posted by: {announcement?.postedBy??[]}</p>
+            </div>
+            <p class="w-full text-center p-4 font-bold text-xl lg:text-2xl">{announcement?.title??[]}</p>
+            <p class="w-full text-center text-sm lg:text-md">{announcement?.content??[]}</p>
+        </section>
+    {/each}
+</div>
