00b677e143a34774604339691d700ac4b41e2d7c
KaiKai-dev <jecalaycay17@gmail.com>
2023-02-05 23:35:40 +0800

    minor changes

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -47,17 +47,17 @@
 						<path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
 					</svg>
 				</button>
-				<button class="dropdown-content menu shadow-lg bg-neutral rounded-box w-max h-max">
+				<ul class="dropdown-content menu shadow-lg bg-primary rounded-box w-max h-max">
 					<li>
-						<button class="flex justify-center hover:bg-neutral hover:underline" on:click={logOutHandler}>
+						<button class="w-full flex justify-center hover:bg-primary hover:underline" on:click={logOutHandler}>
 							<small>Log Out</small>
 						</button>
 					</li>
-				  <li><button class="flex justify-center hover:bg-neutral hover:underline">
+				  <li><button class="flex justify-center hover:bg-primary hover:underline">
 						<small>Account Settings</small>
 				  </button></li>
 				  <!-- <li><a>Item 2</a></li> -->
-				</button>
+				</ul>
 			  </div>
 			{/if}
 		</div>
