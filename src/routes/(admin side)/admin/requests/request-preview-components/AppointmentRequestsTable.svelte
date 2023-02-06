51e8b5ec44b67a5c6d741a7417e8b11abd4da008
KaiKai-dev <jecalaycay17@gmail.com>
2023-02-06 14:41:56 +0800

    seperated date and time

--- src/routes/(admin side)/admin/requests/request-preview-components/AppointmentRequestsTable.svelte
+++ src/routes/(admin side)/admin/requests/request-preview-components/AppointmentRequestsTable.svelte
@@ -19,7 +19,8 @@
                 <th></th>
                 <th>Name</th>
                 <th>Request ID</th>
-                <th>Requested Schedule</th>
+                <th>Requested Date</th>
+                <th>Requested Time</th>
             </tr>
         </thead>
         <tbody>
@@ -28,20 +29,8 @@
                 <th>{index+1}</th>
                 <td>{requestData.lastName}, {requestData.firstName} {requestData.middleName}</td>
                 <td>{requestData.requestId}</td>
-                <td class="flex flex-col gap-2">
-                    <div class="flex items-center gap-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
-                        </svg>
-                        <p>{requestData.appointmentDate}</p>
-                    </div>
-                    <div class="flex items-center gap-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-                        </svg>
-                        <p>{requestData.appointmentTime}</p>
-                    </div>  
-                </td>
+                <td>{requestData.appointmentDate}</td>
+                <td>{requestData.appointmentTime}</td>
             </tr>
             {/each}
         </tbody>
