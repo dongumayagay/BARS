613ad81c36afb7ca02c49f9891469bad395a2abf
KaiKai-dev <jecalaycay17@gmail.com>
2023-02-16 21:19:07 +0800

    changed doc editor main page component name

--- src/routes/(admin side)/admin/management/+page.svelte
+++ src/routes/(admin side)/admin/management/+page.svelte
@@ -4,6 +4,7 @@
     import {onMount} from "svelte"
 	import ManagementHeader from "./ManagementHeader.svelte";
 	import AnnouncementPage from "./make-announcement/AnnouncementPage.svelte";
+	import DocumentsListManagerMainPage from "./documents-list-manager/DocumentsListManagerMainPage.svelte";
 
 
     onMount(()=>{
@@ -17,27 +18,18 @@
     let managementPage = 0;
 
     function navigate(event) {
-     managementPage = event.detail.index;
+        managementPage = event.detail.index;
     }
 </script>
 
-<!-- <main class="w-full max-h-screen flex flex-col items-center p-4 px-1">
-	<ManagementHeader  {managementPage} on:switchTab={navigate}/>
-
-	<div class="w-full h-full bg-neutral min-h-screen flex justify-center pb-[5vh]">
-		<div class="w-[90%] bg-base-100 flex justify-center border-[1px] border-base-300 rounded-lg">
-            <AnnouncementPage  {managementPage} />
-		</div>
-	</div>
-</main> -->
-
-<main class="w-full max-h-screen h-max py-6">
-    <section class="w-full h-full bg-neutral rounded-xl">
+<main class="w-full h-screen py-6">
+    <section class="w-full {managementPage === 1 ? "min-h-[90%] h-full" : "h-max"} bg-neutral rounded-xl">
         <ManagementHeader  {managementPage} on:switchTab={navigate}/>
 
         <div class="w-full h-full bg-neutral min-h-screen flex justify-center pb-[5vh]">
-            <div class="w-[90%] bg-base-100 flex justify-center border-[1px] border-base-300 rounded-lg">
+            <div class="w-[90%] h-full bg-base-100 flex justify-center border-[1px] border-base-300 rounded-lg">
                 <AnnouncementPage  {managementPage} />
+                <DocumentsListManagerMainPage {managementPage} />
             </div>
         </div>
     </section>
