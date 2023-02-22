7401020f346609f4eede1d582ddbebe60c1b7b6d
KaiKai-dev <jecalaycay17@gmail.com>
2023-02-22 18:34:56 +0800

    added notif counter for indicator

--- src/routes/(admin side)/admin/requests/+page.svelte
+++ src/routes/(admin side)/admin/requests/+page.svelte
@@ -9,27 +9,45 @@
     import { userStore, currentPage } from '$lib/stores.js'
 	import { onMount } from 'svelte';
 
-    onMount(()=>{
-        if(!$userStore){
-            goto("../admin")
-        }
-    })
-
     $currentPage = 1;
 
     let page = 0;
 
+    let newAppointmentRequestsCounter = 0;
+    let newDocumentRequestsCounter = 0;
+    let newRequestsCounter
+
+    let newMessages = [];
+
+    onMount(()=>{
+        if(!$userStore){
+            goto("../admin")
+        }
+
+        newAppointmentRequestsCounter = 0;
+        newDocumentRequestsCounter = 0;
+        newMessages = [];
+    })
+
     function navigate(event) {
         page = event.detail.index;
     }
+    $: newRequestsCounter = newAppointmentRequestsCounter + newDocumentRequestsCounter
+    $: console.log(newRequestsCounter)
 </script>
+
 
 <main class="w-full h-full flex flex-col items-center p-4 px-1 ">
 
-	<div class="w-full h-full bg-neutral flex flex-col items-center pb-[5vh] rounded-b-lg">
-        <DAHeader {page} on:switchTab={navigate}/>
+	<div class="w-full h-screen bg-neutral flex flex-col items-center pb-[5vh] rounded-b-lg">
+        <DAHeader {page} {newRequestsCounter} on:switchTab={navigate}/>
 		<div class="w-[90%] h-full bg-base-100 flex justify-center border-[1px] border-base-300 rounded-lg">
-            <PendingList {page} />
+            <PendingList {page} 
+                on:newDocRequest={()=>newDocumentRequestsCounter++} 
+                on:newAptRequest={()=>newAppointmentRequestsCounter++} 
+                on:minusDocNotifCounter={()=>newDocumentRequestsCounter--}
+                on:minusAptNotifCounter={()=>newAppointmentRequestsCounter--}
+            />
             <DocumentsToClaim {page} />
             <ApprovedAppointments {page} />
             <ServedRequestsList {page} />
