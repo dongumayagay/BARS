ab403c33e300af33830b37742d28765ea5e0eddd
KaiKai-dev <jecalaycay17@gmail.com>
2023-02-27 11:20:36 +0800

    fixed loading prompt screen alignment

--- src/routes/(client side)/homepage/hotlines/+page.svelte
+++ src/routes/(client side)/homepage/hotlines/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { collection, getDocs, query } from "firebase/firestore";
     import { db } from "$lib/firebase/client.js"
+    import { Circle } from "svelte-loading-spinners";
 	import HotlineCard from "./HotlineCard.svelte";
 
     async function emergencyHotlinesFetcher() {
@@ -23,7 +24,10 @@
 
 <section class="w-full min-h-screen lg:w-[70%] flex flex-col items-end justify-start gap-6 px-6 pb-6">
     {#await emergencyHotlinesFetcher()}
-        <p>Loading...</p>
+    <div class="w-full h-[70vh] flex flex-col items-center justify-center">
+        <Circle />
+        <p class="">Loading Officials List...</p>
+    </div>
     {:then hotlinesList} 
         {#each hotlinesList as hotline}
             <HotlineCard {hotline}/>
--- src/routes/(client side)/homepage/officials/+page.svelte
+++ src/routes/(client side)/homepage/officials/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { collection, getDocs, onSnapshot, orderBy, query } from "firebase/firestore";
     import { db } from "$lib/firebase/client.js";
+    import { Circle } from "svelte-loading-spinners";
     import Official from "./Official.svelte";
 
     async function officialsListFetcher(){
@@ -23,7 +24,10 @@
 
 <section class="lg:overflow-y-auto lg:max-h-[70vh] w-full lg:w-[70%] flex flex-col lg:flex-auto gap-10 items-center">
     {#await officialsListFetcher()}
-        <p>Loading</p>
+    <div class="w-full h-[70vh] flex flex-col items-center justify-center">
+        <Circle />
+        <p class="">Loading Officials List...</p>
+    </div>
     {:then officialsList} 
         {#each officialsList as official}
             <Official officialProfile={official}/>
