2fc5cde121a6599479560ef5136dfaffdabd066c
KaiKai-dev <jecalaycay17@gmail.com>
2023-03-10 03:45:41 +0800

    lowered the brgy logo opacity to 10%

--- src/routes/(admin side)/admin/management/make-announcement/components/AnnouncementPreviewForm.svelte
+++ src/routes/(admin side)/admin/management/make-announcement/components/AnnouncementPreviewForm.svelte
@@ -60,7 +60,7 @@
 <form class="flex flex-col gap-4" on:submit|preventDefault={submitHandler} on:reset={resetHandler}>
     <section class="bg-neutral w-full lg:w-full p-4 flex flex-col rounded-xl shadow-lg gap-2 relative">
         <div class="absolute top-0 left-0 w-full h-full flex items-center justify-center overflow-hidden z-0">
-            <img src="/brgyLogo.png" alt="brgyLogo" class="w-[75%] lg:w-[50%] opacity-20">
+            <img src="/brgyLogo.png" alt="brgyLogo" class="w-[75%] lg:w-[50%] opacity-10">
         </div>
         <div class="w-full pb-4 flex flex-col border-b-[2px] border-base-100">
             <p class="text-xs lg:text-sm">{Timestamp.now().toDate()}</p>
--- src/routes/(client side)/homepage/Announcement.svelte
+++ src/routes/(client side)/homepage/Announcement.svelte
@@ -26,7 +26,7 @@
 {#each announcements as announcement}
     <section class="bg-neutral w-full p-4 flex flex-col rounded-xl shadow-lg relative">
         <div class="absolute top-0 left-0 w-full h-full flex items-center justify-center overflow-hidden">
-            <img src="/brgyLogo.png" alt="brgyLogo" class="w-[75%] lg:w-[50%] opacity-20">
+            <img src="/brgyLogo.png" alt="brgyLogo" class="w-[75%] lg:w-[50%] opacity-10">
         </div>
         <div class="w-full pb-4 flex flex-col border-b-[2px] border-base-100">
             <p class="text-xs lg:text-sm">{new Timestamp(announcement.datePosted.seconds, announcement.datePosted.nanoseconds).toDate()}</p>
