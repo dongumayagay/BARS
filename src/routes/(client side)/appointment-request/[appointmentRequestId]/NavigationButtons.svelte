1e66e7450c1cd6a138fbdf7c0db46e4c72894f08
KaiKai-dev <jecalaycay17@gmail.com>
2023-03-10 03:48:54 +0800

    added font thickness on request viewer active tabs

--- src/routes/(client side)/appointment-request/[appointmentRequestId]/NavigationButtons.svelte
+++ src/routes/(client side)/appointment-request/[appointmentRequestId]/NavigationButtons.svelte
@@ -8,6 +8,6 @@
 </script>
 
 <nav class="w-full lg:w-[10vw] flex lg:flex-col items-center justify-center gap-3 tabs-boxed bg-inherit rounded-none">
-    <button class="tab h-max {page === 0 ? "tab-active font-semibold " : ""}" on:click={()=>dispatch("navigate", { index: 0})}>Request Details</button>
-    <button class="tab {page === 1 ? "tab-active font-semibold " : ""} " on:click={()=>dispatch("navigate", { index: 1})}>Messages</button>
+    <button class="tab h-max {page === 0 ? "font-bold tab-active" : ""}" on:click={()=>dispatch("navigate", { index: 0})}>Request Details</button>
+    <button class="tab {page === 1 ? "font-bold tab-active" : ""} " on:click={()=>dispatch("navigate", { index: 1})}>Messages</button>
 </nav>
