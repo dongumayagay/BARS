967b495422009bf1ad64d8e80609b2d3cdec2f63
KaiKai-dev <jecalaycay17@gmail.com>
2023-03-14 15:59:51 +0800

    changed the online indicator icon

--- src/lib/components/ProfileIcon.svelte
+++ src/lib/components/ProfileIcon.svelte
@@ -9,8 +9,8 @@
 </script>
 
 <div class="dropdown dropdown-end " class:lg:hidden={$currentInterface === "client" && $userStore === null}>
-    <button class="btn btn-sm w-max h-max indicator" on:click={()=>showDropdown=!showDropdown}>
-        <span class="indicator-item badge badge-success top-[20%] right-[25%] z-0" class:hidden={$userStore == null}></span> 
+    <button class="btn btn-sm w-max h-max relative" on:click={()=>showDropdown=!showDropdown}>
+        <i class="absolute top-[55%] right-[20%] fa-solid fa-circle text-[10px] border-2 rounded-[50%] border-neutral text-success z-0"></i>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
             <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
         </svg>
