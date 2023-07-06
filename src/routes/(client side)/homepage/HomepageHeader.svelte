a38ad562d85307a2610bca58450695ba9ee51f9b
KaiKai-dev <jecalaycay17@gmail.com>
2023-07-07 00:12:16 +0800

    hompage mobile view's menu drawer animation

--- src/routes/(client side)/homepage/HomepageHeader.svelte
+++ src/routes/(client side)/homepage/HomepageHeader.svelte
@@ -8,11 +8,11 @@
     <Links />
 </section>
 
-<section class="lg:hidden fixed top-0 h-[100%] flex items-center {showMenu ? "bg-black/50 w-screen " : ""} z-20">
-    <div class="bg-neutral flex flex-col gap-4 h-screen pt-[20vh] p-4 border-r-2 border-primary" class:hidden={!showMenu}>
+<section class="lg:hidden fixed top-0 h-[100%] flex items-center {showMenu ? "bg-black/50 w-screen " : ""} z-20 transition-all duration-500">
+    <div class="fixed -left-[230px] bg-neutral flex flex-col gap-4 h-screen pt-[20vh] p-4 border-r-2 border-primary {showMenu ? "translate-x-[230px]" : ""} duration-500">
         <Links on:closeMenu={() => showMenu = false} />
     </div>
-    <label class="py-10 bg-neutral rounded-r-xl swap swap-rotate border-2 border-primary border-l-2 border-l-neutral mt-32 {showMenu ? "h-max right-[3.5px]" : ""}">
+    <label class="py-10 bg-neutral rounded-r-xl swap swap-rotate border-2 border-primary border-l-2 border-l-neutral mt-32 {showMenu ? "h-max right-[1px] translate-x-[230px]" : ""} duration-500">
         <input type="checkbox" class="invisible" bind:checked={showMenu} />
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 swap-off text-primary">
             <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5" />
